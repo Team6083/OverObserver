e66a8ce1b4a38881b4f459b2636da295e24608a9
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-03-07 23:53:44 +0800

    Add match list and work good

--- public/js/match_list.js
+++ public/js/match_list.js
@@ -1,54 +1,61 @@
 function addMatchList(match, teams, size) {
   var tr = "<tr>";
   if (size == 0) {
-    tr += "<th scope=\"row\" rowspan=\"2\">" + match + "</th>";
+    tr += "<th scope=\"row\" rowspan=\"2\">" + match.split("_")[1] + "</th>";
     for (var i = 0; i < 3; i++) {
-      console.log(teams.red[i]);
-      if (teams.red[i].finish) {
+      if (teams.red[i.toString()].finish) {
         tr += "<td class=\"table-danger\">";
-        tr += teams.red[i].num;
+        tr += teams.red[i.toString()].num;
         tr += "</td>";
       } else {
         tr += "<td class=\"table-danger font-weight-bold\">";
-        tr += teams.red[i].num;
+        tr += "<a href=\"/teamform.html?team="+teams.red[i.toString()].num+"&match="+match+"\">";
+        tr += teams.red[i.toString()].num;
+        tr += "</a>"
         tr += "</td>";
       }
     }
     tr += "</tr><tr>";
     for (var i = 0; i < 3; i++) {
-      if (teams.blue[i].finish) {
+      if (teams.blue[i.toString()].finish) {
         tr += "<td class=\"table-primary\">";
-        tr += teams.blue[i].num;
+        tr += teams.blue[i.toString()].num;
         tr += "</td>";
       } else {
         tr += "<td class=\"table-primary font-weight-bold\">";
-        tr += teams.blue[i].num;
+        tr += "<a href=\"/teamform.html?team="+teams.blue[i.toString()].num+"&match="+match+"\">";
+        tr += teams.blue[i.toString()].num;
+        tr += "</a>"
         tr += "</td>";
       }
     }
   } else {
     tr += "<th scope=\"row\">";
-    tr += match;
+    tr += match.split("_")[1];
     tr += "</th>"
     for (var i = 0; i < 3; i++) {
-      if (teams.red[i].finish) {
+      if (teams.red[i.toString()].finish) {
         tr += "<td class=\"table-danger\">";
-        tr += teams.red[i].num;
+        tr += teams.red[i.toString()].num;
         tr += "</td>";
       } else {
         tr += "<td class=\"table-danger font-weight-bold\">";
-        tr += teams.red[i].num;
+        tr += "<a href=\"/teamform.html?team="+teams.red[i.toString()].num+"&match="+match+"\">";
+        tr += teams.red[i.toString()].num;
+        tr += "</a>"
         tr += "</td>";
       }
     }
     for (var i = 0; i < 3; i++) {
-      if (teams.blue[i].finish) {
+      if (teams.blue[i.toString()].finish) {
         tr += "<td class=\"table-primary\">";
-        tr += teams.blue[i].num;
+        tr += teams.blue[i.toString()].num;
         tr += "</td>";
       } else {
         tr += "<td class=\"table-primary font-weight-bold\">";
-        tr += teams.blue[i].num;
+        tr += "<a href=\"/teamform.html?team="+teams.blue[i.toString()].num+"&match="+match+"\">";
+        tr += teams.blue[i.toString()].num;
+        tr += "</a>"
         tr += "</td>";
       }
     }
