149af5be05c31fa172bd6c2d051941cab5b6309e
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-08-15 22:13:16 +0800

    Move teamform to firebase database

--- public/js/teamform_config.js
+++ public/js/teamform_config.js
@@ -1,86 +1,22 @@
-var ffield = {
-  'frc_2018': {
-    'year': 2018,
-    'fields': {
-      'auto-scale': {
-        'type': 'int',
-        'mode': 'auto',
-        'displayName': '<span class="badge badge-pill badge-dark">Scale</span>成功次數'
-      },
-      'auto-scale-try': {
-        'type': 'int',
-        'mode': 'auto',
-        'displayName': '<span class="badge badge-pill badge-dark">Scale</span>嘗試次數'
-      },
-      'auto-switch': {
-        'type': 'int',
-        'mode': 'auto',
-        'displayName': '<span class="badge badge-pill badge-dark">Switch</span>成功次數'
-      },
-      'auto-switch-try': {
-        'type': 'int',
-        'mode': 'auto',
-        'displayName': '<span class="badge badge-pill badge-dark">Switch</span>嘗試次數'
-      },
-      'autoLine': {
-        'type': 'boolean',
-        'mode': 'auto',
-        'displayName': 'Auto line',
-        'displayT': '成功',
-        'displayF': '失敗',
-        'Ttag': 'auto-success',
-        'Ftag': 'auto-fail'
-      },
-      'tele-scale': {
-        'type': 'int',
-        'mode': 'teleop',
-        'displayName': '<span class="badge badge-pill badge-dark">Scale</span>成功次數'
-      },
-      'tele-scale-try': {
-        'type': 'int',
-        'mode': 'teleop',
-        'displayName': '<span class="badge badge-pill badge-dark">Scale</span>嘗試次數'
-      },
-      'tele-switch': {
-        'type': 'int',
-        'mode': 'teleop',
-        'displayName': '<span class="badge badge-pill badge-dark">Switch</span>成功次數'
-      },
-      'tele-switch-try': {
-        'type': 'int',
-        'mode': 'teleop',
-        'displayName': '<span class="badge badge-pill badge-dark">Switch</span>嘗試次數'
-      },
-      'tele-exchange': {
-        'type': 'int',
-        'mode': 'teleop',
-        'displayName': '<span class="badge badge-pill badge-dark">Exchange</span>成功次數'
-      },
-      'tele-exchange-try': {
-        'type': 'int',
-        'mode': 'teleop',
-        'displayName': '<span class="badge badge-pill badge-dark">Exchange</span>嘗試次數'
-      },
-      'climb': {
-        'type': 'boolean',
-        'mode': 'teleop',
-        'displayName': '爬升',
-        'displayT': '成功',
-        'displayF': '失敗',
-        'Ttag': 'climb-success',
-        'Ftag': 'climb-fail'
-      },
-      'drive-tech': {
-        'type': 'int',
-        'mode': 'teleop',
-        'displayName': '駕車技術'
-      },
-      'specialThing': {
-        'type': 'textarea',
-        'mode': 'summary',
-        'displayName': '特殊表現',
-        'row': 4
-      }
+function getteamform(eventId, callback){
+  firebase.database().ref("events/" + eventId + "/teamform").once('value').then(function(snapshot) {
+    if (snapshot.exists()) {
+      var data = snapshot.val();
+      callback(data);
     }
-  }
-};
+  });
+}
+
+function reloadNumberBtn() {
+  $(".numInput").bootstrapNumber({
+    upClass: 'primary',
+    downClass: 'danger',
+    center: true
+  });
+}
+
+
+function writeToDB(){
+
+  
+}
