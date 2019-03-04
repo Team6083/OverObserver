5f64147e6a0c2dcccf745b9dcb119038b0524b7d
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-03-04 22:00:06 +0800

    move files

---
+++ public/js/tba_util.js
@@ -0,0 +1,32 @@
+function ajaxEvent(eventKey){
+  return $.ajax({
+    url: "https://www.thebluealliance.com/api/v3/event/"+eventKey+"/simple",
+    accept: 'application/json',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-TBA-Auth-Key", "IcTRNsZoayVbU8wgZ3xRETwnw6O9kvJg4hPC6XXGHbXfDYX8COi1fTW6DCrtBTNy");
+    },
+    type: 'GET'
+  });
+}
+
+function ajaxEventMatchSimple(eventKey) {
+  return $.ajax({
+    url: "https://www.thebluealliance.com/api/v3/event/" + eventKey + "/matches/simple",
+    accept: 'application/json',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-TBA-Auth-Key", "IcTRNsZoayVbU8wgZ3xRETwnw6O9kvJg4hPC6XXGHbXfDYX8COi1fTW6DCrtBTNy");
+    },
+    type: 'GET'
+  });
+}
+
+function ajaxEventTeamsSimple(eventKey) {
+  return $.ajax({
+    url: "https://www.thebluealliance.com/api/v3/event/" + eventKey + "/teams/simple",
+    accept: 'application/json',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("X-TBA-Auth-Key", "IcTRNsZoayVbU8wgZ3xRETwnw6O9kvJg4hPC6XXGHbXfDYX8COi1fTW6DCrtBTNy");
+    },
+    type: 'GET'
+  });
+}
